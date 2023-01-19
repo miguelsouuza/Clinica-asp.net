949ea21f8d4053223e26e11d09f6ae79d5aa88c5
miguelsouuza <69999800+miguelsouuza@users.noreply.github.com>
2023-01-19 15:00:00 -0300

    Add files via upload

--- Tabela.sql
+++ Tabela.sql
@@ -2,51 +2,86 @@ create database dbClinica;
 
 use dbClinica;
 
-create table if not exists tbPaciente(
-codPaciente int primary key auto_increment,
+create table tbPaciente(
+codPaciente int identity,
 nomePaciente varchar(50) not null,
 enderecoPaciente varchar(50),
 telPaciente varchar(14),
 celPaciente varchar(11)
 );
 
-create table if not exists tbEspecialidade(
-codEspecialidade int primary key auto_increment,
+create table tbEspecialidade(
+codEspecialidade int  identity,
 Especialidade varchar(80) not null
 );
 
-create table if not exists tbMedico(
-codMedico int primary key auto_increment,
+create table tbMedico(
+codMedico int  identity,
 nomeMedico varchar(80) not null,
-codEspecialidade int,
-CONSTRAINT foreign key(codEspecialidade) references tbEspecialidade(codEspecialidade)
+cod_Especialidade int
 );
 
-create table if not exists tbAtendimento(
-codAtendimento int primary key auto_increment,
+create table tbAtendimento(
+codAtendimento int identity,
 dataAtendimento varchar(80) not null,
-codPaciente int,
-codMedico int,
-Diagnostico text,
-CONSTRAINT foreign key(codMedico) references tbMedico(codMedico),
-CONSTRAINT foreign key(codPaciente) references tbPaciente(codPaciente)
+cod_Paciente int,
+cod_Medico int,
+Diagnostico varchar(max)
 );
+
+
+/*CREATE PRIMARY KEY*/
+alter table tbPaciente add constraint PK_IDPACIENTE
+PRIMARY KEY (codPaciente)
+go
+
+alter table tbEspecialidade add constraint PK_IDESPECIALIDADE
+PRIMARY KEY (codespecialidade)
+
+ALTER TABLE tbMedico add constraint PK_Medico
+primary key (codMedico)
+go
+
+alter table tbAtendimento add constraint PK_IDATENDIMENTO
+primary key (codAtendimento)
+
+/*CREATE FOREIGN KEY*/
+alter table tbMedico add constraint FK_Especialidade_Medico
+foreign key (cod_Especialidade) references tbEspecialidade(codEspecialidade)
+go
+
+alter table tbAtendimento add constraint FK_PACIENTE_ATENDIMENTO
+foreign key (cod_PACIENTE) references tbPaciente(codPaciente)
+
+alter table tbAtendimento add constraint FK_MEDICO_ATENDIMENTO
+FOREIGN KEY (cod_medico) references tbMedico(codMedico)
+go
+
 
 SELECT tbMedico.codMedico,
 tbMedico.nomeMedico,
 tbEspecialidade.Especialidade
 FROM tbMedico 
-INNER JOIN tbEspecialidade ON tbEspecialidade.codEspecialidade = tbMedico.codEspecialidade;
+INNER JOIN tbEspecialidade ON tbEspecialidade.codEspecialidade = tbMedico.cod_Especialidade;
+
 
 
 select * from tbPaciente;
 
+
 select * from tbPaciente where enderecoPaciente like '%rua%';
+
 select * from tbPaciente where enderecoPaciente like '%@enderecoPaciente';
+
 select * from tbPaciente where enderecoPaciente like '%'+@enderecoPaciente+'%';
+
 select * from tbPaciente where enderecoPaciente like '%''@enderecoPaciente''%';
+
 select * from tbPaciente where enderecoPaciente like '%@enderecoPaciente%';
+
 select * from tbMedico;
+
 select * from tbAtendimento;
+
 select * from tbEspecialidade;
 drop table if exists tbPaciente;
